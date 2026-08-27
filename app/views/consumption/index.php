38bf14eb826ba9e1ee18914cb693caee5b8e9572
arol-git <arolyemeli@gmail.com>
2026-08-27 21:03:40 +0100

    Quiet consumption view diagnostics

--- app/views/consumption/index.php
+++ app/views/consumption/index.php
@@ -7,6 +7,7 @@ $pageScripts = [];
 $selectedData = $selectedData ?? [];
 $selectedMonth = \App\Models\Energy::normalizeMonth((string) ($selectedMonth ?? ''));
 $history = $history ?? [];
+$previousData = $previousData ?? null;
 $changePercent = $changePercent ?? null;
 $monthNames = ['01' => 'Janvier', '02' => 'Février', '03' => 'Mars', '04' => 'Avril', '05' => 'Mai', '06' => 'Juin', '07' => 'Juillet', '08' => 'Août', '09' => 'Septembre', '10' => 'Octobre', '11' => 'Novembre', '12' => 'Décembre'];
 $monthLabel = static function (string $month): string {
